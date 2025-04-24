a36941bf4ea7bebd882680e281bebc1646a7d18f
Chris Gomez <christiangomezdev@gmail.com>
2025-04-25 06:07:52 +0800

    Refactoring baseUrl into .env

--- app/components/mini-ficha/mini-ficha.tsx
+++ app/components/mini-ficha/mini-ficha.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import { Image } from "expo-image";
-import { useGetEmergencyKindsQuery } from '@/app/features/emergency/emergencySlice';
+import { useGetEmergencyKindsQuery } from '@/app/store/emergencySlice';
 import { styles } from "./styles";
 
 
--- app/store/emergencySlice.ts
+++ app/store/emergencySlice.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const emergencyApi = createApi({
   reducerPath: 'emergencyApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://wisemed-interview.s3.us-east-2.amazonaws.com/react-native/' }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: process.env.EXPO_PUBLIC_API_BASE_URL 
+  }),
   endpoints: (builder) => ({
     getEmergencyKinds: builder.query({
       query: () => 'emergency-kinds.json',
