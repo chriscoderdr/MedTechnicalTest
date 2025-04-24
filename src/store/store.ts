3ac799acd115106e017d99ddf23bc60234488ac4
Chris Gomez <christiangomezdev@gmail.com>
2025-04-25 06:43:41 +0800

    Adding patient data to RTK, but as there is not endpoint I'll also add a json data and a hook to get it easy to replace by the actual think

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { emergencyApi } from './emergencySlice';
+import { patientApi } from './patientSlice';
 
 export const store = configureStore({
   reducer: {
     [emergencyApi.reducerPath]: emergencyApi.reducer,
+    [patientApi.reducerPath]: patientApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(emergencyApi.middleware),
+    getDefaultMiddleware()
+      .concat(emergencyApi.middleware)
+      .concat(patientApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
